662a7ad654a382ced2e01388a7f6c7b17ad18d96
Yann Sartori <sartori2@llnl.gov>
2022-04-29 13:40:46 -0700

    Improve path handling

--- .config/zsh/home/path.zsh
+++ .config/zsh/home/path.zsh
@@ -1,6 +1,9 @@
 zsh_plugins="$HOME.config/zsh/plugins"
-export PATH="$zsh_plugins/diff-so-fancy:$PATH"
-export PATH="$HOME/.local/share/nvim/lsp_servers/taplo:$PATH"
-export PATH="$HOME/.poetry/bin:$PATH"
-export PATH="$HOME/.cargo/bin:$PATH"
+path=(
+    "$zsh_plugins/diff-so-fancy:"
+    "$HOME/.local/share/nvim/lsp_servers/taplo"
+    "$HOME/.poetry/bin"
+    "$HOME/.cargo/bin"
+    "$path[@]")
 
+export PATH
